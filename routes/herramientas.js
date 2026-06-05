0837d5262ec155d7c085e537997d8c2cdb6c4187
Fabian Salas <fabianalonzo2006@gmail.com>
2026-06-05 12:13:11 -0500

    Actualizando el m

--- routes/herramientas.js
+++ routes/herramientas.js
@@ -69,8 +69,68 @@ router.get("/:id", async (req, res) => {
 // Registrar
 router.post("/", async (req, res) => {
   try {
+    // Consulta esperada
+    const query = 'INSERT INTO herramientas (nombre, marca, descripcion, condicion, tipo) VALUES (?, ?, ?, ?, ?)';
+
+    //Obtener los datos ... Deserializar objeto. Se debe respetar el orden de los ATRIBUTOS
+    const {nombre, marca, descripcion, condicion, tipo} = req.body;
+
+    //El WS tiene que tener la capacidad de validar
+    if (!nombre || nombre == "") {
+      return res.status(400).json({
+        success: false,
+        message: "Se requiere el nombre de la herramienta",
+      });
+    }
+
+    if (!marca || marca == "") {
+      return res.status(400).json({
+        success: false,
+        message: "Se requiere la marca de la herramienta",
+      });
+    }
+
+    if (!descripcion || descripcion == "") {
+      return res.status(400).json({
+        success: false,
+        message: "Se requiere la descripción de la herramienta",
+      });
+    }
+
+    if (!condicion || condicion == "") {
+      return res.status(400).json({
+        success: false,
+        message: "Se requiere la condición de la herramienta",
+      });
+    }
+
+      if (!tipo || tipo == "") {
+        return res.status(400).json({
+          success: false,
+          message: "Se requiere el tipo de la herramienta",
+        });
+      }
+
+    //Datos requeridos para los comodines
+    const values = [
+      nombre,
+      marca,
+      descripcion,
+      condicion,
+      tipo
+    ];
+
+    // Ejecutar la consulta
+    const [result] = await db.query(query, values);
+
+    //Informar la ejecición der la operación
+    res.status(201).json({
+      success: true,
+      message: "Herramienta registrada correctamente",
+      id: result.insertId, // ID generado por la BD
+    })
+
   } catch (err) {
-    // ¿Por qué 500? Error generado del lado servidor
     res.status(500).json({
       success: false,
       message: "Error en la comunicación al servidor",
@@ -79,9 +139,68 @@ router.post("/", async (req, res) => {
   }
 });
 
-//
-router.put("/", async (req, res) => {
+// Actualizar
+router.put("/:id", async (req, res) => {
   try {
+    // Consulta esperada
+    const query = `UPDATE herramientas SET nombre = ?, marca = ?, descripcion = ?, condicion = ?, tipo = ? WHERE idherramienta = ?`;
+
+    // Existe el id que solicitan actualizar?
+    const [resultHerramientas] = await db.query(
+      "SELECT * FROM herramientas WHERE idherramienta = ?",
+      [req.params.id],
+    );
+
+    if (resultHerramientas.length === 0) {
+      return res.status(404).
+      json({
+        success: false,
+        message: "No encontrado",
+      });
+    }
+
+    // Obtener los datos... deserealizar objeto. Se debe respetar el orden y los nombres de los atributos
+    const { nombre, marca, descripcion, condicion, tipo } = req.body;
+
+    // El WS tiene que tener la capacidad de validar
+    if (!nombre || nombre == "") {
+      return res
+        .status(400)
+        .json({ succcess: false, message: "Se requiere el nombre" });
+    }
+
+    if (!marca || marca == "") {
+      return res
+        .status(400)
+        .json({ succcess: false, message: "Se requiere la marca" });
+    }
+
+    if (!descripcion || descripcion == "") {
+      return res
+        .status(400)
+        .json({ succcess: false, message: "Se requiere la descripción" });
+    }
+
+    if (!condicion || condicion == "") {
+      return res
+        .status(400)
+        .json({ succcess: false, message: "Se requiere la condición" });
+    }
+
+    if (!tipo || tipo == "") {
+      return res
+        .status(400)
+        .json({ succcess: false, message: "Se requiere el tipo" });
+    }
+
+    // Actualizamos el registro => EXISTE + TRAE DATOS
+    const values = [nombre, marca, descripcion, condicion, tipo, req.params.id];
+
+    const [result] = await db.query(query, values);
+    res.json({
+      success: true,
+      message: "Registro actualizado exitosamente",
+    });
   } catch (err) {
     // ¿Por qué 500? Error generado del lado servidor
     res.status(500).json({
