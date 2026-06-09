00efe5d569f6b74ed15204980daf6783d3258849
Fabian Salas <fabianalonzo2006@gmail.com>
2026-06-09 13:44:37 -0500

    Todo

--- database/db.sql
+++ database/db.sql
@@ -9,7 +9,7 @@ CREATE TABLE herramientas (
   descripcion   VARCHAR(100) NOT NULL,
   fotografia     VARCHAR(10) NULL,
   condicion      ENUM('Bueno', 'Regular', 'Malo') NOT NULL DEFAULT 'Bueno',
-  tipo           ENUM('Manual', 'Eléctrica') NOT NULL DEFAULT 'Eléctrica'
+  tipo           ENUM('Manual', 'Electrica') NOT NULL DEFAULT 'Electrica'
 )ENGINE=InnoDB;
 
 INSERT INTO herramientas (nombre, marca, descripcion, tipo) VALUES
@@ -21,3 +21,20 @@ INSERT INTO herramientas (nombre, marca, descripcion) VALUES
 ('Taladro', 'DeWalt', 'Lijadora orbital de 5 pulgadas');
 
 SELECT * FROM herramientas;
+
+-- PROCEDIMIENTO ALMACENATO = PROGRAMA se ejecuta en el motoe de BD
+DELIMITER $$
+CREATE PROCEDURE spu_herramientas_listar()
+BEGIN
+  SELECT * FROM herramientas ORDER BY idherramienta DESC;
+END $$
+
+CALL spu_herramientas_listar();
+
+DELIMITER $$
+CREATE PROCEDURE spu_herramientas_eliminar(IN _idherramienta INT)
+BEGIN
+  DELETE FROM herramientas WHERE idherramienta = _idherramienta;
+END $$
+
+-- CALL spu_herramientas_eliminar(1);
